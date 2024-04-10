d2e631f158a9c8b2ca046dc166c377a22e2489e2
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-10 20:26:37 +0530

    Refactor MovieList component styling and update movie title display

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,14 +4,13 @@ function MovieList({ movies }) {
         <>
             {
                 movies.map((movie, index) => (
-                    <div key={index} className='card movie-card m-2'>
-                        <img src={`https://image.tmdb.org/t/p/w1280/${movie.poster_path}`} className='card-img-top' alt={movie.original_title} />
+                    <div key={index} className='card movie-card m-2 font-monospace bg-success-subtle rounded-5'>
+                        <img src={`https://image.tmdb.org/t/p/w1280/${movie.poster_path}`} className='card-img-top rounded-5' alt={movie.title} />
                         <div className='card-body'>
-                            <h5 className='card-title'>{movie.original_title}</h5>
-                            <p className='card-text text-secondary'>{movie.overview}</p>
+                            <h5 className='card-title fw-bold text-success-emphasis text-decoration-underline'>{movie.title}</h5>
                         </div>
-                        <div className='card-footer'>
-                            <small className='fw-bold text-success'>Rating: {movie.vote_average | Number}/10</small>
+                        <div className='card-footer bg-success-subtle'>
+                            <small className='fw-bold fs-6 text-success-emphasis'>Rating: {movie.vote_average | Number}</small>
                         </div>
                     </div>
                 ))
