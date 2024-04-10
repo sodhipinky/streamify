845c22e2825a158e495141fa1cf57c61cb28cd11
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-10 18:51:00 +0530

    Refactor API fetch calls and data processing to include more results

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,42 @@ function App() {
 
   useEffect(() => {
     Promise.all([
-      fetch('https://api.themoviedb.org/3/discover/movie?api_key=4c0193b45b042c536215774762ee44b5').then((res) => res.json()),
-      fetch('https://api.themoviedb.org/3/discover/movie?page=2&api_key=4c0193b45b042c536215774762ee44b5').then((res) => res.json()),
-      fetch('https://api.themoviedb.org/3/discover/movie?page=3&api_key=4c0193b45b042c536215774762ee44b5').then((res) => res.json()),
-      fetch('https://api.themoviedb.org/3/discover/movie?page=4&api_key=4c0193b45b042c536215774762ee44b5').then((res) => res.json()),
+      fetch('https://api.themoviedb.org/3/discover/movie?api_key=4c0193b45b042c536215774762ee44b5').then((response) => response.json()),
+      fetch('https://api.themoviedb.org/3/discover/movie?page=2&api_key=4c0193b45b042c536215774762ee44b5').then((response) => response.json()),
+      fetch('https://api.themoviedb.org/3/discover/movie?page=3&api_key=4c0193b45b042c536215774762ee44b5').then((response) => response.json()),
+      fetch('https://api.themoviedb.org/3/discover/movie?page=4&api_key=4c0193b45b042c536215774762ee44b5').then((response) => response.json()),
+      fetch('https://api.themoviedb.org/3/discover/movie?page=5&api_key=4c0193b45b042c536215774762ee44b5').then((response) => response.json()),
+      fetch('https://api.themoviedb.org/3/discover/movie?page=6&api_key=4c0193b45b042c536215774762ee44b5').then((response) => response.json()),
+      fetch('https://api.themoviedb.org/3/discover/movie?page=7&api_key=4c0193b45b042c536215774762ee44b5').then((response) => response.json()),
+      fetch('https://api.themoviedb.org/3/discover/movie?page=8&api_key=4c0193b45b042c536215774762ee44b5').then((response) => response.json()),
+      fetch('https://api.themoviedb.org/3/discover/movie?page=9&api_key=4c0193b45b042c536215774762ee44b5').then((response) => response.json()),
+      fetch('https://api.themoviedb.org/3/discover/movie?page=10&api_key=4c0193b45b042c536215774762ee44b5').then((response) => response.json()),
     ])
-      .then(([data1, data2, data3, data4]) => {
-        setMovies([...data1.results, ...data2.results, ...data3.results, ...data4.results]);
+      .then((
+        [
+          page1,
+          page2,
+          page3,
+          page4,
+          page5,
+          page6,
+          page7,
+          page8,
+          page9,
+          page10
+        ]) => {
+        setMovies([
+          ...page1.results,
+          ...page2.results,
+          ...page3.results,
+          ...page4.results,
+          ...page5.results,
+          ...page6.results,
+          ...page7.results,
+          ...page8.results,
+          ...page9.results,
+          ...page10.results
+        ]);
       });
   })
 
