8b5a7824c661b158bdd128185d69dcbb3534db48
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-10 20:50:22 +0530

    Update MovieList component styling and movie card layout

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,13 +4,13 @@ function MovieList({ movies }) {
         <>
             {
                 movies.map((movie, index) => (
-                    <div key={index} className='card movie-card m-2 font-monospace bg-success-subtle rounded-5'>
-                        <img src={`https://image.tmdb.org/t/p/w1280/${movie.poster_path}`} className='card-img-top rounded-5' alt={movie.title} />
+                    <div key={index} className='card movie-card m-2 font-monospace bg-secondary-subtle rounded-5 border-secondary'>
+                        <img src={`https://image.tmdb.org/t/p/w1280/${movie.poster_path}`} className='card-img-top rounded-5 shadow' alt={movie.title} />
                         <div className='card-body'>
-                            <h5 className='card-title fw-bold text-success-emphasis text-decoration-underline'>{movie.title}</h5>
+                            <h5 className='card-title fw-bold text-primary-emphasis text-decoration-underline'>{movie.title}</h5>
                         </div>
-                        <div className='card-footer bg-success-subtle'>
-                            <small className='fw-bold fs-6 text-success-emphasis'>Rating: {movie.vote_average | Number}</small>
+                        <div className='card-footer bg-secondary-subtle'>
+                            <small className='fw-bold fs-6 text-primary-emphasis'>Rating: {movie.vote_average | Number}</small>
                         </div>
                     </div>
                 ))
