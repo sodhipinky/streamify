3d6310a85ae69ac6dcdb1ad9accd05ddf87b8759
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-11 12:46:22 +0530

    Refactor API fetch calls and handle loading and error states in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,62 @@ import MovieList from './components/MovieList';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true); // sets the isLoading state to true
     const fetchPages = async () => {
       const pages = Array.from({ length: 10 }, (_, i) => i + 1); // creates an array [1, 2, ..., 10]
       const allMovies = [];
 
       for (const page of pages) { // fetches movies from pages 1 to 10
-        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?page=${page}&api_key=4c0193b45b042c536215774762ee44b5`);
-        const data = await response.json(); // parses JSON data from the response
-        allMovies.push(...data.results); // appends the movies from the current page to the allMovies array
+        await fetch(`https://api.themoviedb.org/3/discover/movie?page=${page}&api_key=4c0193b45b042c536215774762ee44b5`)
+          .then(response => response.json())
+          .then(pageData => allMovies.push(...pageData.results))
+          .then(() => setMovies(allMovies))
+          .then(() => setIsLoading(false))
+          .catch(error => setError(error));
       }
-
-      setMovies(allMovies); // sets the movies state to the allMovies array
     };
     fetchPages(); // calls the fetchPages function
   }, [])
+
+  if (isLoading) {
+    return (
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col d-flex justify-content-center'>
+            <h1>Loading...</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col d-flex justify-content-center'>
+            <h1>Error: {error.message}</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  if(!movies.length) {
+    return (
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col d-flex justify-content-center'>
+            <h1>No movies found</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div className='container-fluid'>
