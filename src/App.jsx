12a9d4d9df749fb534b55a6797dc72af0bedb5be
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-11 12:00:51 +0530

    Add comments

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,15 @@ function App() {
       const pages = Array.from({ length: 10 }, (_, i) => i + 1); // creates an array [1, 2, ..., 10]
       const allMovies = [];
 
-      for (const page of pages) {
+      for (const page of pages) { // fetches movies from pages 1 to 10
         const response = await fetch(`https://api.themoviedb.org/3/discover/movie?page=${page}&api_key=4c0193b45b042c536215774762ee44b5`);
-        const data = await response.json();
-        allMovies.push(...data.results);
+        const data = await response.json(); // parses JSON data from the response
+        allMovies.push(...data.results); // appends the movies from the current page to the allMovies array
       }
 
-      setMovies(allMovies);
+      setMovies(allMovies); // sets the movies state to the allMovies array
     };
-    fetchPages();
+    fetchPages(); // calls the fetchPages function
   }, [])
 
   return (
