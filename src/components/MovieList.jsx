6585d2f2399771008ac195de7fd1a894490312a1
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-11 14:44:26 +0530

    Update styling in MovieList.jsx and Navbar.jsx

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,7 @@ function MovieList({ movies }) {
                     <div key={index} className='card movie-card m-2 font-monospace bg-secondary-subtle rounded-5 border-secondary shadow'>
                         <img src={`https://image.tmdb.org/t/p/w1280/${movie.poster_path}`} className='card-img-top rounded-5 shadow' alt={movie.title} />
                         <div className='card-body'>
-                            <h5 className='card-title fw-bold text-primary-emphasis text-decoration-underline'>{movie.title}</h5>
+                            <h5 className='card-title fw-bold text-primary-emphasis'>{movie.title}</h5>
                         </div>
                         <div className='card-footer bg-secondary-subtle'>
                             <small className='fw-bold fs-6 text-primary-emphasis'>Rating: {movie.vote_average | Number}</small>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 function Navbar() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary font-monospace mb-3">
+            <nav className="navbar navbar-expand-lg bg-dark font-monospace mb-3">
                 <div className="container-fluid">
-                    <a className="navbar-brand fw-bold fs-2 text-primary-emphasis" href="#">
+                    <a className="navbar-brand fw-bold fs-2 text-white pe-2" href="#">
                         <img src={'../src/assets/favicon.ico'} alt={"Streamify"} width={55} className="d-inline-block me-2" />
                         Streamify</a>
                 </div>
