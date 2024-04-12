2abdf63078594ee7a342499a3baae11faa882652
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-12 12:27:21 +0530

    Add react-circular-progressbar component and update styling in App.css and MovieList.jsx

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,27 @@
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+
 function MovieList({ movies }) {
     return (
         <>
             {
                 movies.map((movie, index) => (
-                    <div key={index} className='card movie-card m-2 font-monospace bg-secondary-subtle rounded-5 border-secondary shadow'>
+                    <div key={index} className='card movie-card m-2 font-monospace border-0 position-relative'>
                         <img src={`https://image.tmdb.org/t/p/w1280/${movie.poster_path}`} className='card-img-top rounded-5 shadow' alt={movie.title} />
-                        <div className='card-footer bg-secondary-subtle'>
-                            <small className='fw-bold fs-6 text-primary-emphasis'>Rating: {movie.vote_average | Number}</small>
+                        <div className='card-body text-start fw-bold'>
+                            <p className='card-title'>{movie.title}</p>
+                        </div>
+                        <div className='rating'>
+                            <CircularProgressbar
+                                value={Math.round(movie.vote_average * 10)}
+                                text={`${Math.round(movie.vote_average * 10)}%`}
+                                styles={buildStyles({
+                                    textColor: "yellow",
+                                    pathColor: "#60e209",
+                                    trailColor: "transparent",
+                                })}
+                            />
                         </div>
                     </div>
                 ))
