3ed04d5620f47fedb7ce0d9785206e8bf65eaa8d
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-12 11:21:34 +0530

    Refactor Header.jsx to store the states and pass them as props to Navbar, and changed the layout of Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,38 @@
 // import Navbar from './Navbar'
 import StreamifyNavbar from "./StreamifyNavbar"
+import { useEffect, useState } from "react"
 
 function Header() {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [genres, setGenres] = useState([]);
+
+    useEffect(() => {
+        const fetchGenres = async () => {
+            await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=4c0193b45b042c536215774762ee44b5')
+                .then(response => response.json())
+                .then(data => setGenres(data.genres))
+                .catch(error => console.error(error));
+        }
+        fetchGenres();
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    });
+
+    const searchFieldWidth = windowWidth <= 992 ? 'w-25' : 'w-75';
+
     return (
         <>
             <div className='container-fluid m-0 p-0 w-100'>
-                <StreamifyNavbar />
+                <StreamifyNavbar
+                    searchFieldWidth={searchFieldWidth}
+                    genres={genres}
+                />
             </div>
         </>
     )
--- src/components/StreamifyNavbar.jsx
+++ src/components/StreamifyNavbar.jsx
@@ -1,33 +1,11 @@
 import Logo from '../assets/favicon.ico'
 import { Container, Nav, Navbar, NavDropdown, Form } from 'react-bootstrap'
-import { useState, useEffect } from 'react'
+import propTypes from 'prop-types'
 
-function StreamifyNavbar() {
-    const [dropdownTitle, ssetDropdownTitle] = useState('All Movies');
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-    const handleClick = (eventKey, event) => {
-        // set the selected optio as the title of the dropdown
-        event.preventDefault();
-        const selectedOption = event.target.innerText;
-        ssetDropdownTitle(selectedOption);
-    };
-
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
-
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    const searchFieldWidth = windowWidth <= 992 ? 'w-25' : 'w-50';
+function StreamifyNavbar({ searchFieldWidth, genres }) {
 
     return (
-        <Navbar bg="dark" expand="lg" variant="dark" className='mb-5 font-monospace'>
+        <Navbar collapseOnSelect bg="dark" variant='dark' expand="lg" className='mb-5 font-monospace'>
             <Container fluid>
                 <Navbar.Brand href="#home">
                     <img src={Logo} alt={"Streamify"} width={55} className="d-inline-block me-2" />
@@ -36,20 +14,27 @@ function StreamifyNavbar() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link className='text-light' href="#home">Home</Nav.Link>
-                        <Nav.Link className='text-light' href="#link">About</Nav.Link>
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">About</Nav.Link>
+                        <NavDropdown title='Movie Lists' id="basic-nav-dropdown" className="align-content-center">
+                            <NavDropdown.Item eventKey={1} defaultValue={"popular"} className='small-font' href="#">Popular</NavDropdown.Item>
+                            <NavDropdown.Item eventKey={2} defaultValue={"top_rated"} className='small-font' href="#">Top Rated</NavDropdown.Item>
+                            <NavDropdown.Item eventKey={3} defaultValue={"now_playing"} className='small-font' href="#">Now Playing</NavDropdown.Item>
+                            <NavDropdown.Item eventKey={4} defaultValue={"upcoming"} className='small-font' href="#">Upcoming</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title='Genre' id="basic-nav-dropdown" className="align-content-center">
+                            {
+                                genres.map((genre, index) => (
+                                    <NavDropdown.Item key={index} eventKey={genre.id} className='small-font' href="#">{genre.name}</NavDropdown.Item>
+                                ))
+                            }
+                        </NavDropdown>
                     </Nav>
                     <Form className="d-flex justify-content-center">
-                        <NavDropdown title={dropdownTitle} id="basic-nav-dropdown" onSelect={handleClick} className="text-light align-content-center ">
-                            <NavDropdown.Item eventKey={0} href="#">All Movies</NavDropdown.Item>
-                            <NavDropdown.Item eventKey={1} href="#">Title</NavDropdown.Item>
-                            <NavDropdown.Item eventKey={2} href="#">Genre</NavDropdown.Item>
-                            <NavDropdown.Item eventKey={3} href="#">Rating</NavDropdown.Item>
-                        </NavDropdown>
                         <Form.Control
                             type="search"
                             placeholder="Search"
-                            className={`ms-2 me-2 rounded-pill border-0 ${searchFieldWidth} small-font`}
+                            className={`ms-2 me-2 rounded-pill border-0 ${searchFieldWidth}`}
                             aria-label="Search"
                         />
                     </Form>
@@ -58,5 +43,11 @@ function StreamifyNavbar() {
         </Navbar>
     )
 }
+
+StreamifyNavbar.propTypes = {
+    searchFieldWidth: propTypes.string.isRequired,
+    genres: propTypes.array.isRequired
+}
+
 
 export default StreamifyNavbar
