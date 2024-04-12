344cb1cb5bf469987747a8fa0eb6398fcef4585d
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-12 21:23:12 +0530

    Refactor trending time period buttons in App.jsx to use smaller button size

--- src/App.jsx
+++ src/App.jsx
@@ -119,9 +119,9 @@ function App() {
                 <div className='col d-flex justify-content-start align-items-center mt-5 ms-3'>
                   <button className='fw-bold rounded-circle scroll-left' onClick={() => scroll(-1, trendingScrollContainer)}>&lt;</button>
                   <p className='d-inline-block fs-4 fw-bold mb-0'>Trending</p>
-                  <div className='col-3 d-flex justify-content-evenly ms-3 align-items-center'>
-                    <button className={`btn ${trendingTimePeriod === 'day' ? 'btn-danger' : 'btn-secondary'} rounded-pill me-1 p-0 w-50`} onClick={() => setTrendingTimePeriod('day')}>Today</button>
-                    <button className={`btn ${trendingTimePeriod === 'week' ? 'btn-danger' : 'btn-secondary'} rounded-pill me-1 p-0 w-50`} onClick={() => setTrendingTimePeriod('week')}>This Week</button>
+                  <div className='col-md-4 d-flex justify-content-evenly ms-3 align-items-center'>
+                    <button className={`btn ${trendingTimePeriod === 'day' ? 'btn-danger' : 'btn-secondary'} rounded-pill me-1 p-1 w-50`} onClick={() => setTrendingTimePeriod('day')}>Today</button>
+                    <button className={`btn ${trendingTimePeriod === 'week' ? 'btn-danger' : 'btn-secondary'} rounded-pill me-1 p-1 w-50`} onClick={() => setTrendingTimePeriod('week')}>This Week</button>
                   </div>
                   <button className='fw-bold rounded-circle scroll-right' onClick={() => scroll(1, trendingScrollContainer)}>&gt;</button>
                 </div>
