3cce21c4f5a71dcadc89b62f33525cbea666a72e
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-12 16:48:41 +0530

    Refactor horizontal scrolling to use separate scroll containers for top rated and trending movies

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,17 @@ import Header from './components/Header';
 function App() {
   const [movies, setMovies] = useState([]);
   const [trendingMovies, setTrendingMovies] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const scrollContainer = useRef(null);
-  const scroll = (direction) => {
-    if (scrollContainer.current) {
-      scrollContainer.current.scrollTo({
-        left: scrollContainer.current.scrollLeft + direction * 300,
+  const topRatedScrollContainer = useRef(null);
+  const trendingScrollContainer = useRef(null);
+
+  const scroll = (direction, ref) => {
+    if (ref.current) {
+      ref.current.scrollTo({
+        left: ref.current.scrollLeft + direction * 300,
         behavior: 'smooth'
       });
     }
@@ -47,6 +50,14 @@ function App() {
         .catch(error => setError(error));
     };
     fetchTrendingMovies();
+
+    const fetchTopRatedMovies = async () => {
+      await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=4c0193b45b042c536215774762ee44b5')
+        .then(response => response.json())
+        .then(data => setTopRatedMovies(data.results))
+        .catch(error => setError(error));
+    };
+    fetchTopRatedMovies();
   });
 
   if (isLoading) {
@@ -88,20 +99,25 @@ function App() {
   return (
     <>
       <Header />
-      <div className='container-fluid font-monospace '>
+      <div className='container font-monospace '>
         <div className='row'>
           <div className='col d-flex justify-content-center ms-4'>
-            <button className='fw-bold rounded-circle mt-5' id="scroll-left" onClick={() => scroll(-1)}>&lt;</button>
-            <h3 className='mt-5 p-3 label rounded-pill'>Trending Movies</h3>
-            <button className='fw-bold rounded-circle mt-5' id="scroll-right" onClick={() => scroll(1)}>&gt;</button>
+            <button className='fw-bold rounded-circle mt-5 scroll-left' onClick={() => scroll(-1, topRatedScrollContainer)}>&lt;</button>
+            <h3 className='mt-5 p-3 label rounded-pill'>Top Rated Movies</h3>
+            <button className='fw-bold rounded-circle mt-5 scroll-right' onClick={() => scroll(1, topRatedScrollContainer)}>&gt;</button>
           </div>
-          <div className='horizontal-scroll mb-0' ref={scrollContainer}>
-            <MovieList movies={trendingMovies} />
+          <div className='horizontal-scroll mb-0' ref={topRatedScrollContainer}>
+            <MovieList movies={topRatedMovies} className="movie-card" />
           </div>
-          <div className='row'>
-            <div className='col d-flex flex-wrap justify-content-start'>
-              <MovieList movies={movies} />
-            </div>
+        </div >
+        <div className='row'>
+          <div className='col d-flex justify-content-center ms-4'>
+            <button className='fw-bold rounded-circle mt-5 scroll-left' onClick={() => scroll(-1, trendingScrollContainer)}>&lt;</button>
+            <h3 className='mt-5 p-3 label rounded-pill'>Trending this week</h3>
+            <button className='fw-bold rounded-circle mt-5 scroll-right' onClick={() => scroll(1, trendingScrollContainer)}>&gt;</button>
+          </div>
+          <div className='horizontal-scroll mb-0' ref={trendingScrollContainer}>
+            <MovieList movies={trendingMovies} className="movie-card" />
           </div>
         </div >
       </div>
