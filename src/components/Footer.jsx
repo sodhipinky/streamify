02a9f8690911196093ec03e59dd0c90fd09457d2
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-17 13:40:03 +0530

    Add Font Awesome icons and update Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,32 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
     return (
         <>
             <div className="container-fluid bg-dark text-light p-3 mt-2 font-monospace">
                 <div className="row">
-                    <div className="col-md-6">
-                        <h5>Contact Us:</h5>
+                    <div className="col-md-6 ms-2">
+                        <h5>Contact:</h5>
                         <p className="fs-5">
                             <a href="mailto:pinky.sodhi@gmail.com" className="text-white">
                                 <FontAwesomeIcon icon={faEnvelope} />
                             </a>
+                            <a href="https://www.linkedin.com/in/pinkysodhi/" className="text-white ms-3">
+                                <FontAwesomeIcon icon={faLinkedin} />
+                            </a>
+                            <a href="https://github.com/sodhipinky" className="text-white ms-3">
+                                <FontAwesomeIcon icon={faGithub} />
+                            </a>
                         </p>
                     </div>
                 </div>
-
+                <div className="row">
+                    <div className="col text-center">
+                        <p>&copy; {new Date().getFullYear()} Streamify. All rights reserved.</p>
+                    </div>
+                </div>
             </div>
         </>
     )
