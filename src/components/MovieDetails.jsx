fa7b43578a84976df0eda651fb94b314818ba6ec
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-18 17:34:13 +0530

    Fix text-wrapping issue in MovieDetails

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -66,7 +66,7 @@ function MovieDetails() {
                 });
         }
         movieDetailsData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [movieId]);
 
     if (!movie) {
@@ -128,11 +128,11 @@ function MovieDetails() {
                                         </CircularProgressbarWithChildren>
                                     </div>
                                 </div>
-                                <p className='card-text fs-5 fw-bold mb-0 mt-3'>Overview</p>
+                                <p className='card-text fs-5 fw-bold mb-0 mt-2'>Overview</p>
                                 <p className="card-text fs-6">
                                     {movie.overview}
                                 </p>
-                                <div className="row mt-4">
+                                <div className="row mt-3">
                                     <div className="col">
                                         <p className="card-text fs-5 mb-0">
                                             {director ? director.name : 'Unknown'}
@@ -165,18 +165,20 @@ function MovieDetails() {
                     </div>
                 </div>
             </div>
-            <div className="d-flex overflow-auto">
-                {movieCredits.cast.slice(0, 10).map(actor => (
-                    <div className="flex-shrink-0" key={actor.id}>
-                        <div className="card h-75 p-5 bg-dark border-0 text-white movie-cast-card border-3 align-items-center text-center">
-                            <img src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} className="card-img-top w-75 rounded-5 border-0" alt={actor.name} />
-                            <div className="card-body">
-                                <h5 className="card-title">{actor.name}</h5>
-                                <p className="card-text">{actor.character}</p>
+            <div className="container" style={{ maxWidth: '100vw' }}>
+                <div className="d-flex overflow-auto">
+                    {movieCredits.cast.slice(0, 10).map(actor => (
+                        <div className="flex-shrink-0" key={actor.id}>
+                            <div className="card h-75 p-5 bg-dark border-0 text-white movie-cast-card border-3 align-items-center text-center">
+                                <img src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} className="card-img-top w-75 rounded-5 border-0" alt={actor.name} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{actor.name}</h5>
+                                    <p className="card-text">{actor.character}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     );
